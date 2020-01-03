ffe3229574db9ab3e94cb0abf88063bed8486fc0
tuffinmuffin <tuffinmuffin@gmail.com>
2020-01-02 23:59:25 -0700

    compelted and escape room example

--- ch3/BulidingEscape/Source/BulidingEscape/OpenDoor.cpp
+++ ch3/BulidingEscape/Source/BulidingEscape/OpenDoor.cpp
@@ -23,65 +23,24 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	Owner = GetOwner();
-	World = GetWorld();
-
-	check(Owner);
-	check(World);
 
 	if (!PressurePlate)
 	{
 		UE_LOG(LogTemp, Error, TEXT("PressurePlate is null"));
 	}
-	//ActorThatOpens = World->GetFirstPlayerController()->GetPawn();
 }
 
 
 void UOpenDoor::openDoor()
 {
-	if (isDoorClosed())
-	{
-		doorOpening = true;
-		doorClosing = false;
-		TargetAngle = OpenedAngle;
-	}
-	if (BP_Enabled)
-	{
-		OnOpenRequest.Broadcast();
-	}
-
-	LastOpenTime = World->GetTimeSeconds();
-
+	OnOpenRequest.Broadcast();
 }
 void UOpenDoor::closeDoor()
 {
-	if (!isDoorClosed())
-	{
-		doorOpening = false;
-		doorClosing = true;
-		TargetAngle = ClosedAngle;
-	}
+	OnCloseRequest.Broadcast();
 
-	if (BP_Enabled)
-	{
-		OnCloseRequest.Broadcast();
-	}
 }
 
-bool UOpenDoor::isDoorClosed()
-{
-	return isTargetClose(ClosedAngle);
-}
-
-bool UOpenDoor::isDoorOpened()
-{
-	return isTargetClose(OpenedAngle);
-}
-
-bool UOpenDoor::isTargetClose()
-{
-	return isTargetClose(TargetAngle);
-}
 
 bool UOpenDoor::isPlateTiggered()
 {
@@ -98,7 +57,7 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 	TArray<AActor*> overlappingActors;
 	PressurePlate->GetOverlappingActors(overlappingActors);
 	float totalMass = 0;
-	for (const auto a : overlappingActors)
+	for (const auto &a : overlappingActors)
 	{
 		auto primitive = a->FindComponentByClass<UPrimitiveComponent>();
 		//UE_LOG(LogTemp, Warning, TEXT("Overlapped by %s"), *a->GetName());
@@ -110,114 +69,18 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 
 }
 
-bool UOpenDoor::isTargetClose(float target)
-{	
-	return FMath::IsNearlyEqual(RelativeRotation.Yaw, target, DoorMinErrorAngle);
-}
-
-bool UOpenDoor::rotateDoor(float deltaAngle)
-{
-
-	//if we are close just return
-	if (isTargetClose())
-	{
-		return false;
-	}
-
-	RelativeRotation.Add(0, deltaAngle, 0);
-	RelativeRotation.Normalize();
-
-	Owner->SetActorRelativeRotation(RelativeRotation);
-
-	//if this move finished the rotation, mark as done
-	if (isTargetClose())
-	{
-		doorOpening = false;
-		doorClosing = false;
-		return false;
-	}
-
-	return true;
-}
-
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	
-	if (!PressurePlate)
+	if (isPlateTiggered())
 	{
-		return;
-	}
-
-	if (BP_Enabled)
-	{
-		if (isPlateTiggered())
-		{
-			openDoor();
-		}
-		else
-		{
-			closeDoor();
-		}
+		openDoor();
 	}
 	else
 	{
-		//Poll trigger
-		//if the actor is in the volume open door.
-		//UE_LOG(LogTemp, Warning, TEXT("Door trigger is %d by %s"), isPlateTiggered(), *ActorThatOpens->GetName());
-		if (isPlateTiggered())
-		{
-			openDoor();
-		}
-		else if (CloseDelaySeconds + LastOpenTime < World->GetTimeSeconds())
-		{
-			closeDoor();
-		}
-
-		AnimateDoor(DeltaTime);
-	}
-	
-
-	//FlapDoor();
-	
-}
-
-void UOpenDoor::FlapDoor()
-{
-	if (isDoorClosed())
-	{
-		UE_LOG(LogTemp, Display, TEXT("Setting Door Opening"));
-		openDoor();
-	}
-
-	if (isDoorOpened())
-	{
-		UE_LOG(LogTemp, Display, TEXT("Setting Door Closing"));
 		closeDoor();
 	}
 }
-
-void UOpenDoor::AnimateDoor(float DeltaTime)
-{
-	float deltaAngle = 0;
-	float direction = OpenClockwise ? 1.0 : -1.0;
-
-	if (doorOpening)
-	{
-		deltaAngle = DoorOpenRateSec * DeltaTime * direction;
-		UE_LOG(LogTemp, Verbose, TEXT("Opening...%f"), deltaAngle);
-	}
-	else if (doorClosing)
-	{
-		deltaAngle = DoorCloseRateSec * DeltaTime * -direction;
-		UE_LOG(LogTemp, Verbose, TEXT("Closing...%f"), deltaAngle);
-	}
-
-	if (doorClosing || doorOpening)
-	{
-		rotateDoor(deltaAngle);
-	}
-}
-
--- ch3/BulidingEscape/Source/BulidingEscape/OpenDoor.h
+++ ch3/BulidingEscape/Source/BulidingEscape/OpenDoor.h
@@ -7,9 +7,8 @@
 #include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCloseRequest);
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -29,32 +28,13 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-	void FlapDoor();
-	void AnimateDoor(float DeltaTime);
 	void openDoor();
 	void closeDoor();
-	bool isDoorClosed();
-	bool isDoorOpened();
-	bool isTargetClose(float target);
-	bool isTargetClose();
 	bool isPlateTiggered();
 	float GetTotalMassOfActorsOnPlate();
 
 private:
-	//user config params
-	UPROPERTY(EditAnywhere)
-	float OpenedAngle = 90.0f;
-	UPROPERTY(EditAnywhere)
-	float ClosedAngle = 0.0f;
-	UPROPERTY(EditAnywhere)
-	float DoorOpenRateSec = 45.0f;
-	UPROPERTY(EditAnywhere)
-	float DoorCloseRateSec = 75.0f;
-	UPROPERTY(EditAnywhere)
-	bool OpenClockwise;
-	UPROPERTY(EditAnywhere)
-	bool BP_Enabled = false;
-	
+
 	//Trigger items
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate;
@@ -62,29 +42,10 @@ private:
 	UPROPERTY(EditAnywhere)
 	float TriggerMass = 60;
 
-	UPROPERTY(EditAnywhere)
-	float CloseDelaySeconds = 1.0;
+	UPROPERTY(BlueprintAssignable)
+	FDoorEvent OnOpenRequest;
 
 	UPROPERTY(BlueprintAssignable)
-	FOnOpenRequest OnOpenRequest;
-
-	UPROPERTY(BlueprintAssignable)
-	FOnCloseRequest OnCloseRequest;
-	
-
-	//door rotation
-	bool doorOpening;
-	bool doorClosing;
-	float TargetAngle = ClosedAngle;
-	FRotator RelativeRotation;
-	float LastOpenTime;
-	
-
-	//house keeping values
-	const float DoorDirection = 1.0;
-	const float DoorMinErrorAngle = 2.0f;
-	AActor* Owner;
-	UWorld* World;
-
+	FDoorEvent OnCloseRequest;
 
 };
